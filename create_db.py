7fc0327a157aa1173567c5e15c83cac1efd96103
Vikrant singh <kingtvarshin1@gmail.com>
2024-02-03 00:37:46 +0530

    sample embedding added

--- create_db.py
+++ create_db.py
@@ -19,17 +19,9 @@ def vector_db_creation():
     text_splitter = CharacterTextSplitter(chunk_size=1000,chunk_overlap=0)
     docs = text_splitter.split_documents(documents)
     # create the open-source embedding function
-    embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    embedding_function = SentenceTransformerEmbeddings(
+        model_name="all-MiniLM-L6-v2",
+        model_kwargs = {'device': 'cpu'}
+    )
     # load it into Chroma
     db = Chroma.from_documents(docs, embedding_function,persist_directory=VECTOR_DB_PATH)
-
-    # query it
-    query = "How to conﬁrm cases of COVID?"
-    res = db.similarity_search_with_score(query,50)
-    score = res[0][1]
-    finale = res[0][0]
-    for i in res:
-        if i[1]>score and i[1]<=1:
-            score = i[1]
-            finale = i[0]
-    print(score,finale)
